d061e7f0f8aa9e578dee5e2d7b50df7d981b8854
HellMem <memo_coronado@hotmail.com>
2019-09-08 13:10:21 -0500

    finished shallow neural network

--- 2_shallow_neural_network/shallow_neural_network.py
+++ 2_shallow_neural_network/shallow_neural_network.py
@@ -77,6 +77,39 @@ def optimization(X, Y, w, b, iterations, learning_rate):
     return parameters, gradients, costs
 
 
+def prediction(X, w, b):
+    A = sigmoid(np.dot(w.T, X) + b)
+    m = X.shape[1]
+    Yp = np.zeroes((1, m))
+
+    for i in range(m):
+        if A[0][i] > 0.5:
+            Yp[0][i] = 1
+
+    return Yp
+
+
+def model(X_train, Y_train, X_test, Y_test, iterations, learning_rate):
+    Xn = X_train.shape[1]
+    w, b = init_parameters(Xn)
+
+    parameters, gradients, costs = optimization(X_train, Y_train, w, b, iterations, learning_rate)
+
+    w = parameters["w"]
+    b = parameters["b"]
+
+    Yp_train = prediction(X_train, w, b)
+    Yp_test = prediction(X_test, w, b)
+
+    print(f"Exactitud (X_train): {100 - np.mean(np.abs(Yp_train - Y_train)) * 100}%")
+    print(f"Exactitud (X_test): {100 - np.mean(np.abs(Yp_test - Y_test)) * 100}%")
+
+    description = {"Costs": costs, "Yp_test": Yp_test, "Yp_train": Yp_train, "w": w, "b": b, "iterations": iterations,
+                   "learning_rate": learning_rate}
+
+    return description
+
+
 if __name__ == "__main__":
     [train_set_x, train_set_y, test_set_x, test_set_y, list_classes] = load_dataset()
 
@@ -100,14 +133,17 @@ if __name__ == "__main__":
     test_set_x = test_set_x.reshape(img_dim * img_dim * 3, -1).T
     test_set_x = test_set_x / 255.
 
-    dim = img_dim * img_dim * 3
-    w, b = init_parameters(dim)
+    description = model(train_set_x, train_set_y, test_set_x, test_set_y, 1000, 0.003)
+    #dim = img_dim * img_dim * 3
+    #w, b = init_parameters(dim)
 
-    X = np.array([[1, 2], [3, 4], [5, 6]])
-    Y = np.array([[1, 0]])
-    w = np.array([[1], [2], [3]])
-    b = 1
-    grads, cost = fb_propagation(X, Y, w, b)
-    print("dw = " + str(grads["dw"]))
-    print("db = " + str(grads["db"]))
-    print("cost = " + str(cost))
+
+
+    #X = np.array([[1, 2], [3, 4], [5, 6]])
+    #Y = np.array([[1, 0]])
+    #w = np.array([[1], [2], [3]])
+    #b = 1
+    #grads, cost = fb_propagation(X, Y, w, b)
+    #print("dw = " + str(grads["dw"]))
+    #print("db = " + str(grads["db"]))
+    #print("cost = " + str(cost))
