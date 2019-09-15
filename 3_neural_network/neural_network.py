f1afdbb48be153a606ddecbb7afd4e42ae5f3f66
HellMem <memo_coronado@hotmail.com>
2019-09-15 13:14:35 -0500

    Added two layer size neural network

---
+++ 3_neural_network/neural_network.py
@@ -0,0 +1,135 @@
+import numpy as np
+import h5py
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
+from scipy import ndimage
+
+
+def load_dataset():
+    np.random.seed(1)
+    m = 400  # número de ejemplos
+    N = int(m / 2)  # número de puntos por clase
+    D = 2  # dimensionalidad
+    X = np.zeros((m, D))  # matriz de datos (cada renglón es un ejemplo)
+    Y = np.zeros((m, 1), dtype='uint8')  # vector de etiquetas ( 1 azul, 0 - rojo)
+    a = 4  # max ray of the flower
+    for j in range(2):
+        ix = range(N * j, N * (j + 1))
+        t = np.linspace(j * 3.12, (j + 1) * 3.12, N) + np.random.randn(N) * 0.2  # theta
+        r = a * np.sin(4 * t) + np.random.randn(N) * 0.2  # radius
+        X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
+        Y[ix] = j
+    X = X.T
+    Y = Y.T
+    return X, Y
+
+
+def layer_sizes_test():
+    np.random.seed(1)
+    X_test = np.random.randn(2, 3)
+    Y_test = np.random.randn(1, 3)
+    return X_test, Y_test
+
+
+def layer_sizes(X, Y):
+    n_x = X.shape[0]
+    n_y = Y.shape[0]
+    n_h = 4
+    return n_x, n_h, n_y
+
+
+def init_parameters(n_x, n_h, n_y):
+    np.random.seed(2)
+    W1 = np.random.randn(n_x, n_h) * 0.01
+    b1 = np.zeros((n_h, 1))
+
+    W2 = np.random.randn(n_h, n_y) * 0.01
+    b2 = np.zeros((n_y, 1))
+
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
+
+    return parameters
+
+
+def sigmoid(x):
+    s = 1 / (1 + np.exp(-x))
+    return s
+
+
+def feed_forward(X, weights, biases):
+    H = X
+    for i in range(len(weights)):
+        weight = weights[i]
+        bias = biases[i]
+        Zi = np.dot(weight.T, H) + bias
+        H = sigmoid(Zi)
+
+    return H
+
+
+def back_prop(X, Y, weights, biases):
+    error_weights = [np.zeros(weight.shape) for weight in weights]
+    error_biases = [np.zeros(bias.shape) for bias in biases]
+
+    for x, y in zip(X.T, Y.T):
+        delta_errors_weights = [np.zeros(weight.shape) for weight in weights]
+        delta_errors_biases = [np.zeros(bias.shape) for bias in biases]
+
+        activation = x
+        activations = [x]
+
+        zs = []
+
+        for b, w in zip(biases, weights):
+            z = np.dot(activation, w) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+
+        delta = (activations[-1] - y) * sigmoid_prime(zs[-1])
+        delta_errors_biases[-1] = delta
+        delta_errors_weights[-1] = np.dot(activations[-2].transpose(), delta)
+
+        error_weights = [ew + dew for ew, dew in zip(error_weights, delta_errors_weights)]
+        error_biases = [eb + deb for eb, deb in zip(error_biases, delta_errors_biases)]
+
+    return error_weights, error_biases
+
+
+def sigmoid_prime(z):
+    return sigmoid(z) * (1 - sigmoid(z))
+
+
+def cost_function(X, Y, weights, biases):
+    cost = 0
+
+    for x, y in zip(X.T, Y.T):
+        predictions = feed_forward(x, weights, biases)
+        predictions = np.square(predictions)
+
+        part1_cost = y * np.log(predictions)
+
+        part2_cost = (1 - y) * np.log(1 - predictions)
+
+        cost += (part1_cost.sum() + part2_cost.sum())
+
+    return -cost / len(X)
+
+
+if __name__ == "__main__":
+    # print(f"n_x={n_x}")
+    # print(f"n_y={n_y}")
+    # print(f"n_h={n_h}")
+    X, Y = load_dataset()
+    X_test, Y_test = layer_sizes_test()
+    n_x, n_h, n_y = layer_sizes(X, Y)
+    parameters = init_parameters(n_x, n_h, n_y)
+    weights = [parameters["W1"], parameters["W2"]]
+    biases = [parameters["b1"], parameters["b2"]]
+
+    feed_forward(X, weights, biases)
+
+    cost_function(X, Y, weights, biases)
+
+    print(back_prop(X, Y, weights, biases))
